008b0f81ed9c7566847440030e123c74a3ddd045
kywsc <fireyw@gmail.com>
2021-07-28 11:58:20 +0900

    개발중

--- src/index.js
+++ src/index.js
@@ -21,11 +21,32 @@ axios.interceptors.response.use(
     });
 
 const bookApi = () =>{
-    return axios.post('book?query=토지',{
-        query:'토지'
+    let searchWord= document.getElementById('bookName').value;
+    searchWord=searchWord? searchWord: '과자';
+    return axios.post(`book?query=${searchWord}`,{
+        // query:'리액트'
     }).then((data)=>{
         console.log('book-data', data)
     });
 }
 
-bookApi()
+function App({$target}){
+    this.$title = document.createElement('h1');
+    this.$titleText= document.createTextNode('책을 찾아보자!');
+    this.$title.appendChild(this.$titleText);
+    $target.appendChild(this.$title)
+
+    this.$bookName= document.createElement('input');
+    this.$bookName.setAttribute("id", "bookName");
+    $target.appendChild(this.$bookName);
+
+    this.$bookSearch= document.createElement('button');
+    this.$bookSearch.innerText='검색';
+    this.$bookSearch.onclick= function(){
+        bookApi();
+    }
+
+    $target.appendChild(this.$bookSearch);
+}
+
+new App({$target:document.querySelector('#main')})
