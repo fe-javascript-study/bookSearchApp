b48a3e7def8a51c842932c375421cd6313376863
kywsc <fireyw@gmail.com>
2021-07-28 20:44:31 +0900

    search for a booklist

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import axios from "axios";
 import API_KEY from "./API_KEY";
 
 axios.interceptors.request.use(
-    (config)=> {
+    (config) => {
         config.baseURL = 'https://dapi.kakao.com/v3/search/'
         config.headers.Authorization = `KakaoAK ${API_KEY}`
         return config;
     },
-     (error) => {
+    (error) => {
         return Promise.reject(error);
     }
 );
@@ -20,33 +20,55 @@ axios.interceptors.response.use(
         return Promise.reject(error);
     });
 
-const bookApi = () =>{
-    let searchWord= document.getElementById('bookName').value;
-    searchWord=searchWord? searchWord: '과자';
-    return axios.post(`book?query=${searchWord}`,{
+const bookApi = () => {
+    let searchWord = document.getElementById('bookName').value;
+    searchWord = searchWord ? searchWord : '과자';
+    return axios.post(`book?query=${searchWord}`, {
         // query:'리액트'
-    }).then((data)=>{
-        console.log('book-data', data)
+    }).then((data) => {
+        let $initBookList = document.getElementById("bookList");
+        $initBookList ? $initBookList.remove(): '';
+
+        let $bookListTemplate =
+            `<div name="bookListTemplate">
+                <img src="{thumbnail}" alt="">
+                <div>저자: {authors}</div>
+                <div>가격: {price}</div>
+                <div>출판사: {publisher}</div>
+            </div>`;
+        let $target = document.querySelector("#main");
+        $target.insertAdjacentHTML('afterend', '<div id="bookList"></div>')
+        const bookList = data.data.documents;
+        bookList.forEach(list => {
+            let $bookList = $bookListTemplate
+                .replace('{thumbnail}', list.thumbnail)
+                .replace('{authors}', list.authors)
+                .replace('{price}', list.price)
+                .replace('{publisher}', list.publisher);
+
+            let $bookTarget = document.querySelector("#bookList");
+            $bookTarget.insertAdjacentHTML("beforeend", $bookList);
+        })
     });
 }
 
-function App({$target}){
+function App({$target}) {
     this.$title = document.createElement('h1');
-    this.$titleText= document.createTextNode('책을 찾아보자!');
+    this.$titleText = document.createTextNode('책을 찾아보자!');
     this.$title.appendChild(this.$titleText);
     $target.appendChild(this.$title)
 
-    this.$bookName= document.createElement('input');
+    this.$bookName = document.createElement('input');
     this.$bookName.setAttribute("id", "bookName");
     $target.appendChild(this.$bookName);
 
-    this.$bookSearch= document.createElement('button');
-    this.$bookSearch.innerText='검색';
-    this.$bookSearch.onclick= function(){
-        bookApi();
+    this.$bookSearch = document.createElement('button');
+    this.$bookSearch.innerText = '검색';
+    this.$bookSearch.onclick = function () {
+        bookApi()
     }
 
     $target.appendChild(this.$bookSearch);
 }
 
-new App({$target:document.querySelector('#main')})
+new App({$target: document.querySelector('#main')})
